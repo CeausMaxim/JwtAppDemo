71e8b7d8739c7da0888e669db5cbe392d4fc3239
CeausMaxim <chaushm@mail.ru>
2021-10-13 20:08:17 +0300

    Add SecurityConfig

---
+++ src/main/java/net/proselyte/jwtappdemo/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package net.proselyte.jwtappdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+}
