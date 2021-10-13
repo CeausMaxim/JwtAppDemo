a2a5b9a02625af86621eaadba1566fb5f720b3f2
CeausMaxim <chaushm@mail.ru>
2021-10-13 19:13:47 +0300

    Updated JwtUserFactory

--- src/main/java/net/proselyte/jwtappdemo/security/jwt/JwtUserFactory.java
+++ src/main/java/net/proselyte/jwtappdemo/security/jwt/JwtUserFactory.java
@@ -1,7 +1,14 @@
 package net.proselyte.jwtappdemo.security.jwt;
 
+import net.proselyte.jwtappdemo.model.Role;
 import net.proselyte.jwtappdemo.model.Status;
 import net.proselyte.jwtappdemo.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class JwtUserFactory {
 
@@ -11,15 +18,22 @@ public final class JwtUserFactory {
 
     public static JwtUser create(User user){
         return new JwtUser(
-                user.getId(),
-                user.getUsername(),
-                user.getFirstName(),
-                user.getLastName(),
-                user.getEmail(),
-                user.getPassword(),
-                null,
-                user.getStatus().equals(Status.ACTIVE),
-                user.getUpdated()
-        );
+                        user.getId(),
+                        user.getUsername(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getEmail(),
+                        user.getPassword(),
+                        user.getStatus().equals(Status.ACTIVE),
+                        user.getUpdated(),
+                        mapToGrantedAuthorities(new ArrayList<>(user.getRoles()))
+                );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> userRoles){
+        return userRoles.stream()
+                .map(role ->
+                        new SimpleGrantedAuthority(role.getName())
+                ).collect(Collectors.toList());
     }
 }
