9c5ef0313e89aa8eed057a0769fda87cf29e0ad6
CeausMaxim <chaushm@mail.ru>
2021-10-13 20:26:32 +0300

    Update AuthenticationRestControllerV1

--- src/main/java/net/proselyte/jwtappdemo/rest/AuthenticationRestControllerV1.java
+++ src/main/java/net/proselyte/jwtappdemo/rest/AuthenticationRestControllerV1.java
@@ -1,10 +1,30 @@
 package net.proselyte.jwtappdemo.rest;
 
+import net.proselyte.jwtappdemo.security.jwt.JwtTokenProvider;
+import net.proselyte.jwtappdemo.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/auth/*")
 public class AuthenticationRestControllerV1 {
-    
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final UserService userService;
+
+    public AuthenticationRestControllerV1(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+    }
+
+    public ResponseEntity login(@RequestBody Object object){
+        return null;
+    }
 }
