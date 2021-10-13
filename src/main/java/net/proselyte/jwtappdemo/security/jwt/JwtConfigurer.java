3c5f98953f2c7a033f743e212b4a71f8569953dc
CeausMaxim <chaushm@mail.ru>
2021-10-13 20:02:06 +0300

    Add JwtConfigurer

---
+++ src/main/java/net/proselyte/jwtappdemo/security/jwt/JwtConfigurer.java
@@ -0,0 +1,9 @@
+package net.proselyte.jwtappdemo.security.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    
+}
