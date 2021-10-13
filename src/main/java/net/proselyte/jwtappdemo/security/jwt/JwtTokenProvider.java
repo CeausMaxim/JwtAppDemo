7a6f47acde02645f9589cf48ae530a17c19aa1f1
CeausMaxim <chaushm@mail.ru>
2021-10-13 19:42:33 +0300

    Updated JwtTokenProvider

--- src/main/java/net/proselyte/jwtappdemo/security/jwt/JwtTokenProvider.java
+++ src/main/java/net/proselyte/jwtappdemo/security/jwt/JwtTokenProvider.java
@@ -7,7 +7,9 @@ import net.proselyte.jwtappdemo.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -58,12 +60,15 @@ public class JwtTokenProvider {
     }
 
     public Authentication getAuthentication(String token){
-
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     public String getUsername(String token){
-
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
     }
+
+    
 
     public boolean validateToken(String token){
 
