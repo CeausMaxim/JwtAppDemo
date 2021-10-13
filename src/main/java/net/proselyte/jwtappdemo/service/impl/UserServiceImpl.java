23de5c813a1052c2e1e89f1bc5de1bdd8f81ede2
CeausMaxim <chaushm@mail.ru>
2021-10-13 15:46:39 +0300

    Updated UserServiceImpl

--- src/main/java/net/proselyte/jwtappdemo/service/impl/UserServiceImpl.java
+++ src/main/java/net/proselyte/jwtappdemo/service/impl/UserServiceImpl.java
@@ -47,21 +47,34 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User> result = userRepository.findAll();
+        log.info("IN getAll - {} users found", result.size());
+        return result;
     }
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        User result = userRepository.findByUsername(username);
+        log.info("IN findByUsername - user: {} found by username: {}", result, username);
+        return result;
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        User result = userRepository.findById(id).orElse(null);
+
+        if (result == null){
+            log.warn("IN findById - no user found by id: {}", id);
+            return null;
+        }
+
+        log.info("IN findById - user: {} found by id: {}", result);
+        return result;
     }
 
     @Override
     public void delete(Long id) {
-
+        userRepository.deleteById(id);
+        log.info("IN delete - user with id: {} successfully deleted");
     }
 }
