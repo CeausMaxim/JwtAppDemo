1a76ab13fa5be09911b18150a13f05e0fbe1f5c9
CeausMaxim <chaushm@mail.ru>
2021-10-13 18:48:40 +0300

    Updated JwtUserDetailsService

--- src/main/java/net/proselyte/jwtappdemo/security/JwtUserDetailsService.java
+++ src/main/java/net/proselyte/jwtappdemo/security/JwtUserDetailsService.java
@@ -1,6 +1,9 @@
 package net.proselyte.jwtappdemo.security;
 
 import lombok.extern.slf4j.Slf4j;
+import net.proselyte.jwtappdemo.model.User;
+import net.proselyte.jwtappdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,8 +13,20 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class JwtUserDetailsService implements UserDetailsService {
 
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(UserService userService){
+        this.userService = userService;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+
+        if (user == null){
+            throw new UsernameNotFoundException("User with username: " + username + " not found");
+        }
         return null;
     }
 }
