d881d65a2bbbfb32626d9af591ee29111fe44aa4
CeausMaxim <chaushm@mail.ru>
2021-10-14 13:14:56 +0300

    Update UserDto

--- src/main/java/net/proselyte/jwtappdemo/dto/UserDto.java
+++ src/main/java/net/proselyte/jwtappdemo/dto/UserDto.java
@@ -2,6 +2,7 @@ package net.proselyte.jwtappdemo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import net.proselyte.jwtappdemo.model.User;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -11,4 +12,26 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private String email;
+
+    public User toUser(){
+        User user = new User();
+        user.setId(id);
+        user.setUsername(username);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+
+        return user;
+    }
+
+    public static UserDto fromUser(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setUsername(user.getUsername());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+
+        return userDto;
+    }
 }
