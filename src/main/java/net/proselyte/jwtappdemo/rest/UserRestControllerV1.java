693cccc04ecb45114e8d41f5157ff9d61ae1705d
CeausMaxim <chaushm@mail.ru>
2021-10-14 14:44:25 +0300

    Update AdminRestControllerV1, Add UserRestControllerV1

--- src/main/java/net/proselyte/jwtappdemo/rest/UserRestControllerV1.java
+++ src/main/java/net/proselyte/jwtappdemo/rest/UserRestControllerV1.java
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/admin/")
-public class AdminRestControllerV1 {
+@RequestMapping(value = "/api/v1/users/")
+public class UserRestControllerV1 {
 
     private final UserService userService;
 
     @Autowired
-    public AdminRestControllerV1(UserService userService) {
+    public UserRestControllerV1(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping(value = "users/{id}")
+    @GetMapping(value = "{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
         User user = userService.findById(id);
 
